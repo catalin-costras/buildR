4b11284634dc47296d1b5762cac3771800a6c2d2
Catalin Costras <47351143+catalin-costras@users.noreply.github.com>
2019-02-27 16:04:52 +0200

    Update virtualbox.sh

--- packer/scripts/virtualbox.sh
+++ packer/scripts/virtualbox.sh
@@ -1,7 +1,7 @@
 VBOX_VERSION=$(cat /home/3pg/.vbox_version)
 
 # required for VirtualBox 4.3.26
-yum install -y bzip2
+yum install -y bzip2 hello-world
 
 cd /tmp
 mount -o loop /home/3pg/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
